1dca4de52b1da3a0f086aa0795bcc015512c5d78
you21979 <you2197901@gmail.com>
2018-04-13 14:14:26 +0900

    mod

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import * as util2 from './lib/jsonrpc2_util'
 import * as type2 from './lib/jsonrpc2_type'
+import * as batch2 from './lib/jsonrpc2_batch'
 export {
     util2,
     type2,
+    batch2,
 }
 
